9929a668126d202a4c2647f1799acfafb8b705cf
刘栋文 <583513107@qq.com>
2018-09-25 13:21:55 +0800

    Rejected commit: 上传检查更新的lib

---
+++ Lib_UpdateAPP/src/main/java/com/allenliu/versionchecklib/utils/FileHelper.java
@@ -0,0 +1,41 @@
+package com.allenliu.versionchecklib.utils;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public class FileHelper {
+
+	public static String getDownloadApkCachePath() {
+
+		String appCachePath = null;
+
+
+		if (checkSDCard()) {
+			appCachePath = Environment.getExternalStorageDirectory() + "/VersionPath/" ;
+		} else {
+			appCachePath = Environment.getDataDirectory().getPath() + "/VersionPath/" ;
+		}
+		File file = new File(appCachePath);
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		return appCachePath;
+	}
+
+
+
+	/**
+	 *
+	 */
+	public static boolean checkSDCard() {
+		boolean sdCardExist = Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED);
+
+		return sdCardExist;
+
+	}
+
+
+
+}
